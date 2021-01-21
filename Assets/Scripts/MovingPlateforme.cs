4882ada052149076d27af98874a81431525e738d
Timothé <rondeautimothe@gmail.com>
2021-01-21 10:50:31 +0100

    plateformeMoving

--- Assets/Scripts/MovingPlateforme.cs
+++ Assets/Scripts/MovingPlateforme.cs
@@ -2,31 +2,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using DG.Tweening;
 public class MovingPlateforme : MonoBehaviour
 {
     private Transform platform;
     public Transform player;
+
+    [Header("Paramètres")]
+    public float posStart = 3;
+    public float posEnd = 3;
     // Start is called before the first frame update
     void Start()
     {
         platform = GetComponent<Transform>();
+        Sequence mySequence = DOTween.Sequence();
+        mySequence.Append(transform.DOMoveX(transform.position.x + posStart, 3));
+        mySequence.AppendCallback(Reloop);
     }
 
-    // Update is called once per frame
-    public void OnTriggerEnter2D(Collider2D other)
+    public void OnCollisionEnter2D(Collision2D other)
     {
-        if (other.GetComponent<Movement>() != null)
+        if (other.collider.CompareTag("Player"))
         {
-            other.transform.SetParent(platform.transform);
+            player.transform.SetParent(platform.transform);
+            Debug.Log("Heloo");
         }
     }
 
-    public void OnTriggerExit2D(Collider2D other)
+    public void OnCollisionExit2D(Collision2D other)
     {
-        if (other.GetComponent<Movement>() != null)
+        if (other.collider.CompareTag("Player"))
         {
-            other.transform.SetParent(null);
+            player.transform.SetParent(null);
         }
+    }
+    public void Reloop()
+    {
+        Sequence mySequence1 = DOTween.Sequence();
+        mySequence1.Append(transform.DOMoveX(transform.position.x - posEnd, 3));
+        mySequence1.AppendCallback(Start);
     }
 }
