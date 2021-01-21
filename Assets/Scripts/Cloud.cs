03d43aa79a27dd33708e145e8091cdfbf705f714
sam <samuel.brb19@gmail.com>
2021-01-21 12:04:47 +0100

    nuage rouge début

--- Assets/Scripts/Cloud.cs
+++ Assets/Scripts/Cloud.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 public class Cloud : MonoBehaviour
 {
     public Animator animator;
-    private bool _bigJump = false;
+    public bool redCloud = false;
     public GameObject player;
+    private bool _startCountdown = false;
+    private float _countdown = 3f;
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         animator.SetBool("CloudCanMove", true);
@@ -25,5 +28,25 @@ public class Cloud : MonoBehaviour
     public void BigJumpFalse()
     {
         player.GetComponent<Movement>().jumpForce -= 10;
+        if (redCloud)
+        {
+            _startCountdown = true;
+            //TODO faire disparaître le nuage
+        }
+    }
+
+    private void Update()
+    {
+        if (_startCountdown)
+        {
+            _countdown -= Time.deltaTime;
+        }
+
+        if (_countdown <= 0)
+        {
+            //TODO faire réapparaître le nuage
+            _startCountdown = false;
+            _countdown = 3f;
+        }
     }
 }
