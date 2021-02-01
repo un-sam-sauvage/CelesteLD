ab659f45a22e2658141b28e866ff63668ba74f3b
Loann <courcoul66@gmail.com>
2021-02-01 19:09:47 +0100

    Push EndExit and Level 5

---
+++ Assets/Scripts/End.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class End : MonoBehaviour
+{
+    public int scene;
+
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(scene);
+        }
+    }
+}
