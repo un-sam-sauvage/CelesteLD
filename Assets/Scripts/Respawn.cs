e04a79e8a8840438279fc74aad389ad1c6e4c666
Timothé <rondeautimothe@gmail.com>
2021-02-01 18:19:06 +0100

    Push_Niv1Good

---
+++ Assets/Scripts/Respawn.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Respawn : MonoBehaviour
+{
+    public Transform respawn;
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            other.gameObject.transform.position = respawn.transform.position;
+        }
+    }
+}
