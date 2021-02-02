ce1365ba70ca159f257efc905c4947c8847ff8c9
Timothé <rondeautimothe@gmail.com>
2021-02-02 18:27:44 +0100

    Platforme_monde7

--- Assets/Scripts/ShieldToActivateDoor.cs
+++ Assets/Scripts/ShieldToActivateDoor.cs
@@ -6,15 +6,17 @@ public class ShieldToActivateDoor : MonoBehaviour
 {
     public int nbShieldRecquire;
     public int nbShieldActivated;
-    
 
+    public Transform point;
     // Update is called once per frame
     void Update()
     {
+        Vector2 move = transform.position;
         if (nbShieldRecquire == nbShieldActivated)
         {
             //TODO porte qui bouge
             Debug.Log("La porte s'ouvre");
+            transform.position = Vector2.MoveTowards(move, point.position, 0.5f);
         }
     }
 }
