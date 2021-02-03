c7703cdb0c6c4dbb940fcd5b1bae777cc36bb7e7
Loann <courcoul66@gmail.com>
2021-02-03 17:29:55 +0100

    Push Level 7

---
+++ Assets/Scripts/SelectLevel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectLevel : MonoBehaviour
+{
+    public void ChoseLevel(int levelNumber)
+    {
+        switch (levelNumber)
+        {
+            case 1:
+                SceneManager.LoadScene(1);
+                break;
+            case 2:
+                SceneManager.LoadScene(2);
+                break;
+            case 3:
+                SceneManager.LoadScene(3);
+                break;
+            case 4:
+                SceneManager.LoadScene(4);
+                break;
+            case 5:
+                SceneManager.LoadScene(5);
+                break;
+            case 6:
+                SceneManager.LoadScene(6);
+                break;
+            case 7:
+                SceneManager.LoadScene(7);
+                break;
+            case 8:
+                SceneManager.LoadScene(8);
+                break;
+        }
+    }
+}
